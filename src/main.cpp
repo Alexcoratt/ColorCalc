fbac58ce1c61579f2f0107ae3ebd35459d44bc67
Alexander Smirnov <alexsm.w@yandex.ru>
2023-10-01 15:49:55 +0300

    Updated makefile and main

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,14 @@
 #include <string>
 
 #include "MathParser.hpp"
+#include "Environment.hpp"
 
 int main() {
     std::string line = "77 * 7 / 3 + (23.07 * 0.3) -153";
     std::cout << line << std::endl;
 
-    MathParser prs;
+    Environment * env = new Environment;
+    MathParser prs(env);
     std::cout << prs.parseString(line) << std::endl;
 
     return 0;
